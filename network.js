44bacbde8d6b622d96ba9d671c80962b5e147c88
QiaLin <qia@eskuad.com>
2024-03-23 14:31:58 -0400

    update

--- network.js
+++ network.js
@@ -1,7 +1,7 @@
 class NeuralNetwork{
     constructor(neuronCounts){
         this.levels=[];
-        //因为每层有输入层和输出层 最后一层没有输出层必须减一
+        //因为每层有输入层和输出层 奇数时最后一层有输入层但没有输出层所以必须减一
         for(let i =0;i<neuronCounts.length-1;i++){
             this.levels.push(new Level(
                 neuronCounts[i],neuronCounts[i+1]
